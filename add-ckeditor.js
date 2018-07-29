1389a7cd768b0e6fd46c16d05b381b102dc2574b
star zhang <zhangmx.g@gmail.com>
2018-07-29 21:12:27 +0800

    ckeditor 引用问题

---
+++ add-ckeditor.js
@@ -0,0 +1,5 @@
+var oHead = document.getElementsByTagName('HEAD').item(0),
+oScript = document.createElement("script");
+oScript.type = "text/javascript";
+oScript.src = "/assets/js/ckeditor.js";
+oHead.appendChild(oScript);
